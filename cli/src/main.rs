e81182dd15d4efb243745d6b889658eb3344476f
Divkov575 <dmitriy@ivkov.net>
2024-07-28 12:19:38 -0400

    'seperated files'

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,32 +1,11 @@
-// #![feature(fs_try_exists)]
-use std::{fs, thread::panicking};
-use anyhow::{Context, Result};
+use anyhow::{Result};
+use clap;
+mod config_parsing;
+mod config;
 
-fn create_config_dir(file_path: String) -> Result<()>  {
-    let path = std::path::Path::new(&file_path);
-    if path.exists() {
-        if path.is_file() {
-            panic!("File exists in place of dir");
-        }
-        let files = path.read_dir()?;
 
-        for file in files {
-           let a = file.unwrap().file_name();
-            print!("{:#?}", a);
-        }
-        
-        
-
-    }
-
+fn main() -> Result<()> {
+    config_parsing::handle_creation("../test")?;
     
-    
-
-
-
     Ok(())
-}
-
-fn main() {
-    create_config_dir("test".to_string()).unwrap();
 }
