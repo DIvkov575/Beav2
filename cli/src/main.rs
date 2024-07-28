e2f707df2d6440131296f2b931bd7f20f499ef77
Divkov575 <dmitriy@ivkov.net>
2024-07-28 16:23:49 -0400

    'successfully added incomplete validator'

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,43 +1,23 @@
-mod config_parsing;
-mod config;
+// mod config_parsing;
+// mod config;
 mod commands;
 
-use anyhow::{Result};
+use anyhow::Result;
 use clap::{self, Parser};
-use commands::Command;
+// use exitcode::ExitCode;
 
 
-#[derive(Debug, Parser)]
-#[command(
-    version,
-    about,
-    long_about = None,
-    after_help = "Note: `trash -h` prints a short and concise overview while `trash --help` gives all \
-                 details.",
-)]
-pub struct Args {
-    /// The command to run.
-    #[clap(subcommand)]
-    pub command: Option<Command>,
-
-    // #[clap(flatten)]
-    // config_args: ConfigArgs,
-
-    // #[clap(flatten)]
-    // put_args: put::PutArgs,
-}
-impl Args {
-    pub fn run(self) -> Result<()> {
-        match self.command {
-            None => self.put_args.run(&self.config_args)?,
-            Some(command) => command.run(&self.config_args)?,
-        }
-        Ok(())
-    }
-}
 
 fn main() -> Result<()> {
-    config_parsing::handle_creation("../test")?;
-    
+    // match try_main() {
+    //     Ok(()) => ExitCode::Success.exit(),
+    //     Err(e) => ExitCode::Error.exit_with_msg(format!("{e:#}")),
+    // }
+
+    commands::Args::parse().run()?;
     Ok(())
 }
+
+// fn try_main() -> Result<()> {
+//     commands::Args::parse().run()
+// }
