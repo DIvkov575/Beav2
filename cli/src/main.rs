9e77b36b8c7934f0c73a48d9aba14bab7474b3d2
Divkov575 <dmitriy@ivkov.net>
2024-07-28 13:20:30 -0400

    'cli improvements'

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,8 +1,40 @@
-use anyhow::{Result};
-use clap;
 mod config_parsing;
 mod config;
+mod commands;
 
+use anyhow::{Result};
+use clap::{self, Parser};
+use commands::Command;
+
+
+#[derive(Debug, Parser)]
+#[command(
+    version,
+    about,
+    long_about = None,
+    after_help = "Note: `trash -h` prints a short and concise overview while `trash --help` gives all \
+                 details.",
+)]
+pub struct Args {
+    /// The command to run.
+    #[clap(subcommand)]
+    pub command: Option<Command>,
+
+    // #[clap(flatten)]
+    // config_args: ConfigArgs,
+
+    // #[clap(flatten)]
+    // put_args: put::PutArgs,
+}
+impl Args {
+    pub fn run(self) -> Result<()> {
+        match self.command {
+            None => self.put_args.run(&self.config_args)?,
+            Some(command) => command.run(&self.config_args)?,
+        }
+        Ok(())
+    }
+}
 
 fn main() -> Result<()> {
     config_parsing::handle_creation("../test")?;
